0688fb0b21f2be7851bfcd5e4afaa6e24cc00e75
someone <someone@someoneplace.com>
2022-04-30 15:09:49 +0700

    add shell script for automatic project installation

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get install libboost-all-dev
+
+sudo apt-get install qt5-default
+
+if ! [ -d ./build]; then
+  rm -R ./build
+fi
+
+mkdir ./build && cd ./build
+
+testing_status=false
+while [ -n "$1" ]
+do
+case "$1" in
+  -t)
+    cmake .. -DBUILD_TESTING=ON
+    testing_status=true
+  ;;
+  *)
+    echo "$1 is not an option"
+  ;;
+esac
+shift
+done
+
+if [ "$testing_status" ]; then
+  cmake ..
+fi
+
+cmake --build .
